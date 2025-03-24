78e3bb9a2b06bb4638d4f82e9d84e9e50b2cc9a0
tharu62 <tharinduxdeshan@gmail.com>
2025-03-24 22:40:58 +0100

    changes

---
+++ include/collision&merge.hpp
@@ -0,0 +1,57 @@
+#ifndef COLLISION_MERGE_HPP
+#define COLLISION_MERGE_HPP
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include "celestial_body.hpp"
+#include "planet.hpp"
+#include "sun.hpp"
+#include "black_hole.hpp"
+
+/**
+ * @brief Check if two celestial bodies have collided
+ */
+bool collided(Celestial_body &a, Celestial_body &b)
+{
+    double distance = sqrt(pow(a.get_position()[0] - b.get_position()[0], 2) + pow(a.get_position()[1] - b.get_position()[1], 2) + pow(a.get_position()[2] - b.get_position()[2], 2));
+    if(distance < a.get_radius() + b.get_radius()){
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Merge two celestial bodies
+ */
+Celestial_body merge(Celestial_body &a, Celestial_body &b, type t)
+{ 
+    Celestial_body c;
+
+    switch (t){
+    case planet:
+        c = Planet();
+        break;
+    case sun:
+        c = Sun();
+        break;  
+    case black_hole:
+        c = Black_hole();
+        break;
+    default:
+        Celestial_body c;
+        break;
+    }
+
+    c.set_mass(a.get_mass() + b.get_mass());
+    // c.set_radius(); boh
+    // c.set_color(); boh
+
+    c.set_position({(a.get_position()[0] + b.get_position()[0])/2, (a.get_position()[1] + b.get_position()[1])/2, (a.get_position()[2] + b.get_position()[2])/2});
+    // c.set_velocity(); boh
+    // c.set_acceleration(); boh
+    return c;
+
+}
+
+#endif // COLLISION_MERGE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,14 @@ using namespace std;
 
 int main() 
 {
-    
-    Planet earth;
     auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
     window.setFramerateLimit(144);
+    
+    sf::CircleShape circle;
+    circle.setRadius(1);
+    circle.setOutlineColor(sf::Color::Red);
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition({10, 20});
 
     while (window.isOpen())
     {
@@ -22,13 +26,19 @@ int main()
             {
                 window.close();
             }
+
+            // code to handle simulation and drawing on window
+
+            
         }
 
+        // draw after clearing the window
         window.clear();
+        window.draw(circle);
         window.display();
     }
     
-    cout << "Hello, World!" << endl;
+    cout << "Window closed!" << endl;
     cout << "SFML Version: " << SFML_VERSION_MAJOR << "." << SFML_VERSION_MINOR << endl;
     return 0;
 }

b2a2e4877879d9e1c4ca9668df1d0f0d100e421a
tharu62 <tharinduxdeshan@gmail.com>
2025-03-27 12:35:15 +0100

    First complete version of simulator (with mouse scaling)

--- include/application.hpp
+++ include/application.hpp
@@ -13,7 +13,8 @@
 #include "black_hole.hpp"
 #include "eventHandler.hpp"
 
-#define GALAXY_DIMENSION 700
+#define GALAXY_DIMENSION 100
+// #define G 6.672*pow(10,-11)
 
 class Application{
 
@@ -21,12 +22,10 @@ class Application{
     unsigned int width;
     unsigned int height;
 
-
-
     private:
 
     /**
-     * @brief Initialization of the galaxy with a random numeber of planets, suns and black holes.
+     * @brief Initialization of the galaxy with a random numeber of planets and a suns or black holes at the center.
      */
     void setUp(Celestial_body *galaxy, sf::CircleShape *circle){
     
@@ -34,41 +33,39 @@ class Application{
         float rand_2;
         srand(time(0));
 
-        for(int i = 0; i < GALAXY_DIMENSION; ++i){
+        // Black hole in the center of the screen
+        Black_hole temp = Black_hole();
+        temp.set_mass();
+        temp.set_radius(5);
+        temp.set_position({620, 360});
+        temp.prev_position = temp.position;
+        temp.set_acceleration({0, 0});
+        temp.set_color();
+        galaxy[0] = temp;
+
+        // Sun in the center of the screen
+        // Sun temp = Sun();
+        // temp.set_mass();
+        // temp.set_radius(109);
+        // temp.set_position({620, 360});
+        // temp.prev_position = temp.position;
+        // temp.set_acceleration({0, 0});
+        // temp.set_color();
+        // galaxy[0] = temp;
+
+        for(int i = 1; i < GALAXY_DIMENSION; ++i){
             // int seed = rand()%100;
             rand_1 = rand()%1280;
             rand_2 = rand()%720;
 
-            // if(seed == 0 || seed > 4){
-                Planet temp = Planet();
-                temp.set_mass();
-                temp.set_radius(2);
-                temp.set_position({rand_1, rand_2});
-                temp.prev_position = temp.position;
-                temp.set_acceleration({0, 0});
-                temp.set_color();
-                galaxy[i] = temp;
-            // }
-            // if(seed == 1 || seed == 2 || seed == 3){
-            //     Sun temp = Sun();
-            //     temp.set_mass();
-            //     temp.set_radius(109);
-            //     temp.set_position({rand_1, rand_2});
-            //     temp.prev_position = temp.position;
-            //     temp.set_acceleration({0, 0});
-            //     temp.set_color();
-            //     galaxy[i] = temp;
-            // }
-            // if(seed == 4){
-            //     Black_hole temp = Black_hole();
-            //     temp.set_mass();
-            //     temp.set_radius(rand()%2);
-            //     temp.set_position({rand_1, rand_2});
-            //     temp.prev_position = temp.position;
-            //     temp.set_acceleration({0, 0});
-            //     temp.set_color();
-            //     galaxy[i] = temp;
-            // }
+            Planet temp = Planet();
+            temp.set_mass();
+            temp.set_radius(2);
+            temp.set_position({rand_1, rand_2});
+            temp.prev_position = temp.position;
+            temp.set_acceleration({0, 0});
+            temp.set_color();
+            galaxy[i] = temp;
         }
 
         for(int i = 0; i < GALAXY_DIMENSION; ++i){
@@ -81,8 +78,68 @@ class Application{
             } 
             circle[i].setFillColor(galaxy[i].color);
             circle[i].setPosition(galaxy[i].position);
+            circle[i].setOrigin({galaxy[i].radius, galaxy[i].radius});
+        }
+    }
+
+    /**
+     * @brief Variant of function setUp with all celestial bodies in random positions.
+     */
+    void setUp_rand(Celestial_body *galaxy, sf::CircleShape *circle){
+    
+        float rand_1;
+        float rand_2;
+        srand(time(0));
+
+        for(int i = 1; i < GALAXY_DIMENSION; ++i){
+            int seed = rand()%100;
+            rand_1 = rand()%1280;
+            rand_2 = rand()%720;
+
+            if(seed == 0 || seed > 2){
+                Planet temp = Planet();
+                temp.set_mass();
+                temp.set_radius(2);
+                temp.set_position({rand_1, rand_2});
+                temp.prev_position = temp.position;
+                temp.set_acceleration({0, 0});
+                temp.set_color();
+                galaxy[i] = temp;
+            }
+            if(seed == 1){
+                Sun temp = Sun();
+                temp.set_mass();
+                temp.set_radius(109);
+                temp.set_position({rand_1, rand_2});
+                temp.prev_position = temp.position;
+                temp.set_acceleration({0, 0});
+                temp.set_color();
+                galaxy[i] = temp;
+            }
+            if(seed == 2){
+                Black_hole temp = Black_hole();
+                temp.set_mass();
+                temp.set_radius(5);
+                temp.set_position({rand_1, rand_2});
+                temp.prev_position = temp.position;
+                temp.set_acceleration({0, 0});
+                temp.set_color();
+                galaxy[i] = temp;
+            }
         }
 
+        for(int i = 0; i < GALAXY_DIMENSION; ++i){
+            circle[i].setRadius(galaxy[i].radius);
+            if(galaxy[i].type == black_hole){
+                circle[i].setOutlineThickness(0.6);
+                circle[i].setOutlineColor(sf::Color::White);
+            }else{
+                circle[i].setOutlineColor(galaxy[i].color);
+            } 
+            circle[i].setFillColor(galaxy[i].color);
+            circle[i].setPosition(galaxy[i].position);
+            circle[i].setOrigin({galaxy[i].radius, galaxy[i].radius});
+        }
     }
 
     /**
@@ -90,7 +147,7 @@ class Application{
      */
     void update_position(Celestial_body &body, sf::CircleShape &circle){
 
-        float time_delta = 0.01;
+        float time_delta = 0.0083;
         sf::Vector2f temp = body.position;
 
         body.position.x = (temp.x * 2) - body.prev_position.x + (body.acceleration.x * pow(time_delta, 2));
@@ -108,7 +165,7 @@ class Application{
 
         double F;           // Gravitational Force as scalar
         double r_2;         // Square of the distance between celestial bodies
-        float magnitude;
+        double magnitude;
         sf::Vector2f direction;
     
         for(int i=0; i < GALAXY_DIMENSION; ++i){
@@ -116,23 +173,22 @@ class Application{
     
                 r_2 = pow(galaxy[i].position.x - galaxy[j].position.x, 2) + pow(galaxy[i].position.y - galaxy[j].position.y, 2);
                 
-                if(r_2 != 0.0){
+                if(r_2 != 0.0 && sqrt(r_2) >= (galaxy[i].radius+galaxy[j].radius)){
                     F = (galaxy[i].mass + galaxy[j].mass) / r_2;
                     direction = galaxy[j].position - galaxy[i].position;
                     magnitude = sqrt(direction.x * direction.x + direction.y * direction.y);
                     if (magnitude != 0) {
-                        direction /= magnitude; // Normalize the vector
+                        direction /= (float) magnitude; // Normalize the vector
                     }
                     galaxy[i].acceleration.x += direction.x * (F/galaxy[i].mass);
                     galaxy[i].acceleration.y += direction.y * (F/galaxy[i].mass);
                 }
+                // std::cout << "dir " << direction.x << " " << direction.y << std::endl;
                 
             }
         }
     
     }
-
-
 
 
 
@@ -146,12 +202,15 @@ class Application{
 
     ~Application(){ std::cout << "Simulator deconstracted!" << std::endl; }
 
+    /**
+     * @brief Main loop for simulation of celestial bodies and graphical output on window.
+     */
     void run(){
         std::cout << "Simulator open!" << std::endl;
 
         auto window = sf::RenderWindow(sf::VideoMode({width, height}), "Gravity Simulator");
         sf::View view(sf::FloatRect({0.f, 0.f}, {1280.f, 720.f}));
-        window.setFramerateLimit(60);
+        window.setFramerateLimit(120);
         
         Celestial_body galaxy[GALAXY_DIMENSION];
         sf::CircleShape circle[GALAXY_DIMENSION];
@@ -163,12 +222,7 @@ class Application{
             while (const std::optional event = window.pollEvent())
             {
     
-                EventHandler(event, view);
-    
-                if (event->is<sf::Event::Closed>())
-                {
-                    window.close();
-                }
+                EventHandler(event, view, window);
     
             }
     
@@ -182,8 +236,10 @@ class Application{
             // draw after clearing the window
             window.clear();
             for(int i = 0; i < GALAXY_DIMENSION; ++i){
+                
                 window.setView(view);
                 window.draw(circle[i]);
+
             }
             window.display();
         }
--- include/eventHandler.hpp
+++ include/eventHandler.hpp
@@ -4,25 +4,37 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
-void EventHandler(std::optional<sf::Event> event, sf::View &view){
+void EventHandler(std::optional<sf::Event> event, sf::View &view, sf::Window &window){
 
-    if(event->is<sf::Event::MouseWheelScrolled>()){
-        view.zoom(-2);
-        std::cout << "zoom down" << std::endl;
+    if (const auto* mouseWheelScrolled = event->getIf<sf::Event::MouseWheelScrolled>()){
+        
+        if(mouseWheelScrolled->delta > 0){
+            view.zoom(0.5);
+            // std::cout << "wheel type: vertical" << std::endl;
+
+        }else{
+            view.zoom(2);
+            // std::cout << "wheel type: horizontal" << std::endl;
+        }
     }
+
     if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()){
         
         if (keyPressed->scancode == sf::Keyboard::Scan::U){
 
             view.zoom(2);
-            std::cout << "zoom up" << std::endl;
+            // std::cout << "zoom up" << std::endl;
         }
 
         if (keyPressed->scancode == sf::Keyboard::Scan::D){
 
             view.zoom(0.5);
-            std::cout << "zoom up" << std::endl;
+            // std::cout << "zoom down" << std::endl;
         }
+    }
+    
+    if (event->is<sf::Event::Closed>()){
+        window.close();
     }
 
 }
